8273641bb5e8955e5167a751dcb3fbf4c10f09c9
inno-cent <ekweozorinnocent@gmail.com>
2024-10-08 19:03:41 +0100

    feat : token update 2

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,21 +1,9 @@
-const jwt = require("jsonwebtoken");
-const user = require("../models/User");
+const jwt = require('jsonwebtoken');
 
-exports.protect = async (req, res, next) => {
-  let token;
-
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startswith("Bearer")
-  ) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select("-password");
-      next();
-    } catch (error) {
-      res.status(401).json({ message: "Not authorized, token failed" });
-    }
-  }
-  if (!token) return res.status(401).json({ message: 'Not authorized, no token' });
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d', // Token valid for 30 days
+  });
 };
+
+module.exports = generateToken;

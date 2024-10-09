0d22b11e9326fcf302e29e509a0a9a9fd7b46139
inno-cent <ekweozorinnocent@gmail.com>
2024-10-09 20:10:38 +0100

    fix : mongoose fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);

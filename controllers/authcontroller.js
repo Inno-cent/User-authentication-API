66ec2983f1c7b8b1985a5153c7fc81cbda2ef9d0
inno-cent <ekweozorinnocent@gmail.com>
2024-10-09 23:35:18 +0100

    chore : used try/catch

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -6,42 +6,53 @@ const generateToken = require("../utils/generateToken");
 
 exports.registerUser = async (req, res) => {
   const { name, email, password } = req.body;
-  const userExists = await User.findOne({ email });
-
-  if (userExists)
-    return res.status(400).json({ message: "User already exists" });
 
   if (!name || !email || !password) {
     return res.status(400).json({ message: "Please fill out all inputs" });
   }
+  try {
+    const userExists = await User.findOne({ email });
 
-  const user = await User.create({ name, email, password });
-  if (user) {
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(400).json({ message: "Invalid user data" });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+
+    const user = await User.create({ name, email, password });
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
   }
 };
 
+// login controller
+
 exports.loginUser = async (req, res) => {
-  console.log("Login function called with email:", req.body.email);
-
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
 
-  if (user && (await user.comparePassword(password))) {
-    res.json({
-      _id: user._id,
-      username: user.username,
-      email: user.email,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(401).json({ message: "Invalid email or password" });
+  try {
+    const user = await User.findOne({ email });
+
+    if (user && (await user.comparePassword(password))) {
+      res.json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } catch {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
   }
 };

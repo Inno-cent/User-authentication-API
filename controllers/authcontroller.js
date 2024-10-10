ab4f0ee45851bfa74846dd1d0eec8e1d38b48b46
inno-cent <ekweozorinnocent@gmail.com>
2024-10-10 08:29:13 +0100

    fix: importoed module

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const user = require("../models/User");
+const crypto = require('crypto');
+
 const generateToken = require("../utils/generateToken");
 
 // Register new user controller
@@ -52,6 +54,41 @@ exports.loginUser = async (req, res) => {
       res.status(401).json({ message: "Invalid email or password" });
     }
   } catch {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+// Forgot password controller
+exports.forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Generate a reset token
+    const resetToken = crypto.randomBytes(32).toString("hex");
+    user.resetPasswordToken = resetToken;
+    user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+    await user.save();
+
+    // Send email
+    const mailOptions = {
+      to: user.email,
+      subject: "Password Reset",
+      text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n` +
+            `Please click on the following link to reset your password:\n` +
+            `http://localhost:3000/reset-password/${resetToken}\n\n` +
+            `If you did not request this, please ignore this email.\n`,
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.json({ message: "Email sent with password reset link" });
+  } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser } = require("../controllers/authcontroller");
+const {
+  registerUser,
+  loginUser,
+  forgotPassword,
+} = require("../controllers/authcontroller");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/forget-password", forgotPassword);
 
-module.exports = router
+module.exports = router;

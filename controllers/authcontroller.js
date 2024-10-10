84098df577902725b2a8f4eb9e9598ccf66fd66f
inno-cent <ekweozorinnocent@gmail.com>
2024-10-10 18:48:35 +0100

    fix: nodemailer

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,32 +1,21 @@
 const User = require("../models/User");
-const user = require("../models/User");
-const crypto = require('crypto');
-const nodemailer = require('nodemailer');
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
 const generateToken = require("../utils/generateToken");
-
-
-// Set up nodemailer transporter
-// const transporter = nodemailer.createTransport({
-  // Change as per your email service
-//   service: "Gmail", 
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
 
 // Set up test nodemailer transporter using Ethereal
 const createTestTransporter = async () => {
   const testAccount = await nodemailer.createTestAccount();
 
   return nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
+    host: "smtp.ethereal.email",
     port: 587,
-    secure: false, // true for 465, false for other ports
+    // true for 465, false for other ports
+    secure: false,
     auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass  // generated ethereal password
-    }
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
   });
 };
 
@@ -85,7 +74,6 @@ exports.loginUser = async (req, res) => {
   }
 };
 
-
 // Forgot password controller
 exports.forgotPassword = async (req, res) => {
   const { email } = req.body;
@@ -107,16 +95,17 @@ exports.forgotPassword = async (req, res) => {
     const mailOptions = {
       to: user.email,
       subject: "Password Reset",
-      text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n` +
-            `Please click on the following link to reset your password:\n` +
-            `http://localhost:3000/reset-password/${resetToken}\n\n` +
-            `If you did not request this, please ignore this email.\n`,
+      text:
+        `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n` +
+        `Please click on the following link to reset your password:\n` +
+        `http://localhost:3000/reset-password/${resetToken}\n\n` +
+        `If you did not request this, please ignore this email.\n`,
     };
 
     const info = await transporter.sendMail(mailOptions);
-    
+
     // Log preview URL to console
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    console.log("Preview Forgot Password URL: %s", nodemailer.getTestMessageUrl(info));
 
     res.json({ message: "Email sent with password reset link" });
   } catch (error) {
--- models/User.js
+++ models/User.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     resetPasswordToken: String,
     resetPasswordExpires: Date,
